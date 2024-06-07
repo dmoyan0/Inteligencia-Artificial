b255cd4431c2c96dbec64bf845fb3a553e60b9a5
dmoyan0 <diego.moyano@usm.cl>
2024-06-06 21:15:15 -0400

    .

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -6,6 +6,7 @@
 #include <set>
 #include <string>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -142,7 +143,61 @@ map<string, vector<Sala>> countExams(const string& nombreArchivo) {
 
     return examSala;
 }
+//Funcion para leer los examenes
+map<int, int> cantExamenes(const string& nombreArchivo) {
+    ifstream archivo(nombreArchivo);
+    map<int, int> examenes;
 
+    if (archivo.is_open()) {
+        int idExamen, cant;
+        while (archivo >> idExamen >> cant) {
+            examenes[idExamen] = cant;
+        }
+        archivo.close();
+    } else {
+        cerr << "Error al abrir el archivo " << nombreArchivo << endl;
+    }
+
+    return examenes;
+}
+//GREEDY
+map<int, int> greedyScheduler(const vector<vector<int>>& matrizConflictos, const map<int, int>& examenes) {
+    vector<int> exámenesOrdenados;
+    for (const auto& [idExamen, _] : examenes) {
+        exámenesOrdenados.push_back(idExamen);
+    }
+
+    // Ordenar los exámenes por el número de conflictos (grado)
+    sort(exámenesOrdenados.begin(), exámenesOrdenados.end(), [&matrizConflictos](int a, int b) {
+        int gradoA = count(matrizConflictos[a].begin(), matrizConflictos[a].end(), 1);
+        int gradoB = count(matrizConflictos[b].begin(), matrizConflictos[b].end(), 1);
+        return gradoA > gradoB;
+    });
+
+    map<int, int> asignacion; // examen -> time-slot
+    int timeSlot = 0;
+
+    for (int examen : exámenesOrdenados) {
+        bool asignado = false;
+        while (!asignado) {
+            bool conflicto = false;
+            for (const auto& [examenAsignado, ts] : asignacion) {
+                if (ts == timeSlot && matrizConflictos[examen][examenAsignado] == 1) {
+                    conflicto = true;
+                    break;
+                }
+            }
+            if (!conflicto) {
+                asignacion[examen] = timeSlot;
+                asignado = true;
+            } else {
+                timeSlot++;
+            }
+        }
+    }
+
+    return asignacion;
+}
 //Main
 int main() {
     string archivoEstudiantes = "./Carleton91.stu";
@@ -151,9 +206,9 @@ int main() {
     //Constantes
     //Matriz de conflictos
     vector<vector<int>> matrizConflictos = crearMatrizConflictos(archivoEstudiantes);//C 
-    //int N = UINTMAX_MAX;//Numero muy grande
     set<string> A = leerEstudiantes(archivoEstudiantes);//Conjunto de nombres de los alumnos
-    set<string> E = leerExamenes(archivoEstudiantes);//Conjunto de nombres de los alumnos, cada indice corresponde a la sala del examen
+    set<string> E = leerExamenes(archivoEstudiantes);//Conjunto de nombres de los Examenes
+    map<int, int> examenes = cantExamenes(archivoExamenes);//Leer los examenes y sus cantidades
     string T;//Cantidad de bloques en los que se deben realizar los exámenes
     string D;//Bloques por dia
     vector<int> W = {16, 8, 4, 2, 1, 0};
@@ -162,6 +217,7 @@ int main() {
     //Variables
     map<string, vector<Sala>> examSala = countExams(archivoEstudiantes);//Salas y capacidad que utilizan los examen
 
+    map<int, int> asignacion = greedyScheduler(matrizConflictos, examenes);
     // for (const auto& pair : examSala) {
     //     string examen = pair.first;
     //     vector<Sala> salas = pair.second;
@@ -171,9 +227,9 @@ int main() {
     //     }
     // }
 
-    for (const auto& estudiante : E) {
-        cout << estudiante << endl;
-    }
+    //for (const auto& estudiante : E) {
+    //    cout << estudiante << endl;
+    //}
     // // Imprimir la matriz de conflictos
     // cout << "Matriz de Conflictos:" << endl;
     // for (size_t i = 0; i < matrizConflictos.size(); ++i) {
@@ -182,6 +238,16 @@ int main() {
     //     }
     //     cout << endl;
     // }
+
+    ofstream archivo("./Carleton91.sol");
+    if (archivo.is_open()){
+        for (const auto& [examen, timeSlot] : asignacion){
+            archivo << examen << " " << timeSlot << endl;
+        }
+        archivo.close();
+    } else {
+        cerr << "Error al abrir el archivo " << "Carleton91.sol" << endl;
+    }
 
     return 0;
 
